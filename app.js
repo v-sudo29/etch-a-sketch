deac7821bb036a4c6a1e6b78567c680184c089f1
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-02-16 12:29:45 -0800

    Fix mouseover after changing grids

--- app.js
+++ app.js
@@ -1,35 +1,63 @@
 // Create 16x16 grid of square divs
 const container = document.querySelector('.container');
 
+let number = 16;
 
-for (let i = 0; i < 256; i++) {
+// Function to create a grid of any number of squares
+function createGrid(number) {
+    height = 320 / number;
+    width = 320 / number;
 
-    // Create a grid square and append to container
-    let div = document.createElement('div');
-    div.setAttribute('class', 'grid');
+    for (let i = 0; i < number * number; i++) {
+        
+
+        // Create a grid square and append to container
+        let div = document.createElement('div');
+        div.setAttribute('class', 'grid');
+        
+        container.appendChild(div);
     
-    container.appendChild(div);
-
-    // Style square grid
-    div.style.height = '20px';
-    div.style.width = '20px';
-    div.style.border = '1px solid black';
+        // Style square grid
+        div.style.height = height + 'px';
+        div.style.width = width + 'px';
+        div.style.border = '1px solid black';
+    }
 }
 
-// Use .forEach to iterate over each grid square
-const grids = document.querySelectorAll('.grid');
+// Create default grid
+let defaultGrid = createGrid(number);
+
+// Button event
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+
+    // Prompt user to give an acceptable number
+    do {
+        number = prompt('Number');
+    } while (number <= 2 || number > 16);
+
+    // Remove existing grid
+    let currentGrids = document.querySelectorAll('.grid');
+    currentGrids.forEach((grid) => {
+        grid.remove();
+    });
+
+    // Add new grid
+    createGrid(number);
+    currentGrids = document.querySelectorAll('.grid');
+    currentGrids.forEach((grid) => {
+        grid.addEventListener('mouseover', () => {
+            grid.style.backgroundColor = 'black';
+         });
+    });
+});
+
+// DEFAULT: Use .forEach to iterate over each grid square
+let grids = document.querySelectorAll('.grid');
 
 grids.forEach((grid) => {
     grid.addEventListener('mouseover', () => {
        grid.style.backgroundColor = 'black';
     });
 });
-
-// Button event
-const button = document.querySelector('button');
-let number = 16;
-
-button.addEventListener('click', () => {
-    number = prompt("Number of pixels on one side");
-});
-
