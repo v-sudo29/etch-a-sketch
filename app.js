d65ea80300e1e1784e7f7ba1cdaaf8910c875532
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-02-16 11:00:30 -0800

    Add button to ask for number of squares

--- app.js
+++ app.js
@@ -3,7 +3,33 @@ const container = document.querySelector('.container');
 
 
 for (let i = 0; i < 256; i++) {
+
+    // Create a grid square and append to container
     let div = document.createElement('div');
-    div.setAttribute('id', 'grid');
+    div.setAttribute('class', 'grid');
+    
     container.appendChild(div);
+
+    // Style square grid
+    div.style.height = '20px';
+    div.style.width = '20px';
+    div.style.border = '1px solid black';
 }
+
+// Use .forEach to iterate over each grid square
+const grids = document.querySelectorAll('.grid');
+
+grids.forEach((grid) => {
+    grid.addEventListener('mouseover', () => {
+       grid.style.backgroundColor = 'black';
+    });
+});
+
+// Button event
+const button = document.querySelector('button');
+let number = 16;
+
+button.addEventListener('click', () => {
+    number = prompt("Number of pixels on one side");
+});
+
