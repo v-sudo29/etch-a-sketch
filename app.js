1d627c9f3da5ddc1fa7e89eb2d06edcd2e99b301
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-02-16 08:56:26 -0800

    Create 256 grid squares

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const container = document.querySelector('.container');
 
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
     div.setAttribute('id', 'grid');
     container.appendChild(div);
